9a165a8ac14813a5923564bb22cd1a96d1513ae4
Liqing Li <53241017+Liqingcathy@users.noreply.github.com>
2022-11-19 21:23:16 -0800

    Update and rename RecommendationRunner.java to RecommenderInterfaceImp./RecommendationRunner.java

--- RecommenderInterfaceImp./RecommendationRunner.java
+++ RecommenderInterfaceImp./RecommendationRunner.java
@@ -1,14 +1,7 @@
 import java.util.*;
-/**
- * Write a description of RecommendationRunner here.
- * 
- * @author (your name) 
- * @version (a version number or a date)
- */
+
 public class RecommendationRunner implements Recommender {
-    /*
-     * returns a list of strings representing movie IDs that will be used to present movies to the user for them to rate. 
-     */
+    //returns a list of strings representing movie IDs that will be used to present movies to the user for them to rate. 
     public ArrayList<String> getItemsToRate (){
        ArrayList<String> movieID = new ArrayList<>();
        MovieDatabase.initialize("/Users/liqingli/Desktop/转码/4.Project/data/ratedmoviesfull.csv");
