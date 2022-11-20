321b1636ad2d01bd126a2d2d338d2d436ebfe54b
Liqing Li <53241017+Liqingcathy@users.noreply.github.com>
2022-11-19 20:53:55 -0800

    Add files via upload

---
+++ LoadMoviesFromCSV/Rater.java
@@ -0,0 +1,17 @@
+
+import java.util.ArrayList;
+
+/**
+ * Write a description of Rater here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public interface Rater {
+    public void addRating(String item, double rating);
+    public boolean hasRating(String item) ;
+    public String getID() ;
+    public double getRating(String item) ;
+    public int numRatings() ;
+    public ArrayList<String> getItemsRated();
+}
