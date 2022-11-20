1e8d2b7317b32cb012265c362383cb733e7119be
Liqing Li <53241017+Liqingcathy@users.noreply.github.com>
2022-11-19 21:12:15 -0800

    Update and rename DirectorsFilter.java to FilterInterfaceImplementation/DirectorsFilter.java

--- FilterInterfaceImplementation/DirectorsFilter.java
+++ FilterInterfaceImplementation/DirectorsFilter.java
@@ -1,13 +1,10 @@
 
 /**
- * Create a new class named DirectorsFilter​that implements Filter. 
  * The constructor should have one parameter named directors​representing a list of directors 
  * separated by commas (Example: "Charles Chaplin,Michael Mann,Spike Jonze", 
  * and its satisfies​method should return true if a movie has at least one of these directors as one of its directors. 
  * Note that each movie may have several directors.
  * 
- * @author (your name) 
- * @version (a version number or a date)
  */
 public class DirectorsFilter implements Filter{
     private String director;
